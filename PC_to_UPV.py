4b372ae02ae199413eae476cdec2833b96240816
Tahfiz Tahar <tahfiz_tiger@hotmail.com>
2025-07-02 09:52:19 +0800

    test

---
+++ PC_to_UPV.py
@@ -0,0 +1,8 @@
+import pyvisa
+
+# # Connect to UPV via TCP/IP or USB/GPIB
+# rm = pyvisa.ResourceManager()
+# upv = rm.open_resource("TCPIP0::192.168.1.100::inst0::INSTR")
+
+
+print("PyVISA is installed correctly.")
